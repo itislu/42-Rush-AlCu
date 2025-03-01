fb924f267d20bfe1ed04653823676967798d81ff
Michael Weghofer <mweghofe@c4r7p6.42vienna.com>
2025-03-01 23:33:25 +0100

    print board clear lines fixed

--- src/print_board.c
+++ src/print_board.c
@@ -3,11 +3,11 @@
 #include "libft.h"
 
 static void print_board_row(t_board *board, size_t row);
-static void clear_complete_board(t_board *board, int player_offset);
+static void clear_complete_board(t_board *board, int offset);
 
 void print_board_gameloop(t_board *board, unsigned int pieces)
 {
-	int	player_offset = 0;
+	int	offset = 0;
 	static size_t row_prev_turn;
 	static bool	init_done;
 	bool row_change = false;
@@ -16,35 +16,42 @@ void print_board_gameloop(t_board *board, unsigned int pieces)
 	if (!init_done) {
 		row_prev_turn = board->cur_row;
 		print_board_complete(board);
-		ft_printf("\nAI removed %i pieces\n", pieces);
+		if (pieces > 1)
+			ft_printf("\nAI removed %i pieces\n\n", pieces);
+		else
+			ft_printf("\nAI removed %i piece\n\n", pieces);
 		init_done = true;
 		return ;
 	}
 	// determine the correct offset (what is printed after the board)
 	Player cur_player = board->rows[board->cur_row]->last_pick;
 	if (cur_player == PLAYER) {
-		player_offset = PLAYER_OFFSET;
+		offset = PLAYER_OFFSET;
 	}
 	// offset change when line is finished
 	if (row_prev_turn != board->cur_row) {
+		offset++;
 		row_change = true;
-		player_offset++;
-		if (board->rows[board->cur_row]->last_pick == PLAYER)
-			row_prev_turn = board->cur_row;
+		row_prev_turn = board->cur_row;
 	}
 	// replace only last line or the whole board
 	if (!row_change) {
-		// print_clear_changed_row(board, player_offset);
-		clear_rows(player_offset + 1);
+		clear_rows(offset + 1);
 		print_board_row(board, board->cur_row);
-		// ft_printf("it worked!\n");
 	}
 	else {
-		clear_complete_board(board, player_offset);
+		clear_complete_board(board, offset);
 		print_board_complete(board);
 	}
-	if (cur_player == AI)
-		ft_printf("\nAI removed %i pieces\n", pieces);
+	if (cur_player == AI) {
+		if (pieces > 1)
+			ft_printf("\nAI removed %i pieces\n\n", pieces);
+		else
+			ft_printf("\nAI removed %i piece\n\n", pieces);
+	}
+	// adjustment for las player move
+	else if (board->cur_row == 0 && board->rows[0]->cur_amount == 0)
+		ft_printf("\n");
 }
 
 
@@ -108,7 +115,7 @@ void print_board_complete(t_board *board)
 	}
 }
 
-static void clear_complete_board(t_board *board, int player_offset)
+static void clear_complete_board(t_board *board, int offset_in)
 {
 	size_t curr_height;
 	size_t offset = 1;
@@ -129,5 +136,5 @@ static void clear_complete_board(t_board *board, int player_offset)
 	// removes one more line when the last line is empty
 	
 	curr_height = ft_min(curr_height, PRT_ROW_LIMIT);
-	clear_rows(curr_height + offset + player_offset);
+	clear_rows(curr_height + offset + offset_in);
 }
