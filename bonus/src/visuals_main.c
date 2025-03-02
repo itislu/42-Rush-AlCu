319e3d5c9e1d53ac8aac89df4256de4d758d1d69
icseri <icseri@student.42vienna.com>
2025-03-02 14:57:07 +0100

    ncurses visuals basic structure added

---
+++ bonus/src/visuals_main.c
@@ -0,0 +1,36 @@
+#include "../inc/visuals.h"
+// here to shutup the blue squiggles...
+#include <ncurses.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void	test_draw(t_ncurses *env)
+{
+	// sample text inside all windows
+	mvwprintw(env->board.win, 1, 1, "Board");
+	mvwprintw(env->history.win, 1, 1, "History");
+	mvwprintw(env->input.win, 1, 1, "Input");
+	// draw the windows
+	wrefresh(env->board.win);
+	wrefresh(env->history.win);
+	wrefresh(env->input.win);
+}
+
+// void	run_visuals(t_board *board)
+void	run_visuals(void)
+{
+	t_ncurses	env;
+
+	setup_ncurses(&env);
+	// inital draw windows?
+	test_draw(&env);
+	// some loop?
+	sleep(2);
+	cleanup_ncruses(&env);
+}
+
+int main(void)
+{
+	run_visuals();
+	return (0);
+}
---
+++ bonus/src/visuals_setup.c
@@ -0,0 +1,77 @@
+#include "../inc/visuals.h"
+// here to shutup the blue squiggles...
+#include <ncurses.h>
+#include <stdlib.h>
+
+static void	init_ncurses(void)
+{
+	setlocale(LC_ALL, "");
+    initscr();
+    noecho();
+    cbreak();
+    curs_set(0);	// hide cursor
+    // keypad(stdscr, TRUE); // we use windows, don't need that, this is for the terminal
+    mousemask(ALL_MOUSE_EVENTS, NULL);
+}
+
+static void	setup_windows_sizes(t_ncurses *env)
+{
+	env->board.size.x = env->term.x * 2 / 3 - 1; // -1 for the border not to overlap
+	env->board.size.y = env->term.y - 1; // this might be the missing 1 for the +3 offset
+	env->history.size.x = env->term.x / 3;
+	env->history.size.y = env->term.y * 2 / 3 - 1;
+	env->input.size.x = env->term.x / 3;
+	env->input.size.y = env->term.y / 3 + 1;
+}
+
+static void	setup_windows_position(t_ncurses *env)
+{
+	env->board.pos.x = 1;
+	env->board.pos.y = 1;
+	env->history.pos.x = env->term.x * 2 / 3;
+	env->history.pos.y = 1;
+	env->input.pos.x = env->term.x * 2 / 3;
+	env->input.pos.y = env->term.y * 2 / 3;
+}
+
+static bool	init_windows_err(t_ncurses *env)
+{
+	getmaxyx(stdscr, env->term.y, env->term.x);
+	setup_windows_sizes(env);
+	setup_windows_position(env);
+	env->board.win = newwin(env->board.size.y, env->board.size.x, env->board.pos.y, env->board.pos.x);
+	if (!env->board.win)
+		return (true);
+	env->history.win = newwin(env->history.size.y, env->history.size.x, env->history.pos.y, env->history.pos.x);
+	if (!env->history.win)
+		return (true);
+	env->input.win = newwin(env->input.size.y, env->input.size.x, env->input.pos.y, env->input.pos.x);
+	if (!env->input.win)
+		return (true);
+	// draw the borders for the windows, uses TOP and BOTTOM line for the border
+	box(env->board.win, 0, 0);
+	box(env->history.win, 0, 0);
+	box(env->input.win, 0, 0);
+	// activate keyboard input for the input window
+	keypad(env->input.win, TRUE);
+	return (false);
+}
+void	cleanup_ncruses(t_ncurses *env)
+{
+	if (env->board.win)
+		delwin(env->board.win);
+	if (env->history.win)
+		delwin(env->history.win);
+	if (env->input.win)
+		delwin(env->input.win);
+	endwin();
+}
+
+void	setup_ncurses(t_ncurses *env)
+{
+	init_ncurses();
+	if (init_windows_err(env)) { // needs proper game exit
+		cleanup_ncruses(env);
+		//exit(1);
+	}
+}
