8c9e6e181d9f305c0cba1d377aff851308c723a8
icseri <icseri@student.42vienna.com>
2025-03-02 14:57:07 +0100

    ncurses visuals basic structure added

---
+++ bonus/inc/visuals.h
@@ -0,0 +1,28 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h> // Include this for setlocale
+
+typedef struct s_size{
+	int	x;
+	int	y;
+} t_size;
+
+typedef struct s_win {
+	WINDOW	*win;
+	t_size	size;
+	t_size	pos;
+	int		scroll_offset;
+}	t_win;
+
+typedef struct s_ncurses {
+	t_win	board;
+	t_win	history;
+	t_win	input;
+	t_size	term;
+}	t_ncurses;
+
+void	setup_ncurses(t_ncurses *env);
+void	cleanup_ncruses(t_ncurses *env);
