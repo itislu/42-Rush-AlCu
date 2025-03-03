55f010b72b64ddaa1699707e172cf17f5a6dfd20
mweghofe <mweghofe@c4r7p6.42vienna.com>
2025-03-03 09:09:34 +0100

    feat: new look for board /w long lines
    changed board print logic to allow printing of any overlength line replacement like
     '|||| ... |||||||'

--- bonus/src/visuals_main.c
+++ bonus/src/visuals_main.c
@@ -5,19 +5,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// static void	test_draw(t_ncurses *env)
-// {
-// 	// sample text inside all windows
-// 	mvwprintw(env->board.win, 1, 1, "Board");
-// 	mvwprintw(env->history.win, 1, 1, "History");
-// 	mvwprintw(env->input.win, 1, 1, "Input");
-// 	// draw the windows
-// 	wrefresh(env->board.win);
-// 	wrefresh(env->history.win);
-// 	wrefresh(env->input.win);
-
-// }
-
 // void	handle_input_window(t_ncurses *env, int key)
 // {
 // 	// get the next move thing
@@ -30,10 +17,11 @@
 
 void update_board(t_win *n_board, t_board *board, int offset)
 {
-	int xoffset = 1;
-	int yoffset = 1;
-	int char_limit = 0;
-	int text_len = 0;
+	unsigned int xoffset = 1;
+	unsigned int yoffset = 1;
+	unsigned int text_len = 0;
+	unsigned int stop_x = 0;
+	unsigned int max_char_x = 0;
 	size_t i = 0;
 	t_row *row = NULL;
 
@@ -46,61 +34,31 @@ void update_board(t_win *n_board, t_board *board, int offset)
 	while (i <= board->cur_row - offset) {
 		xoffset = 1;
 		row = board->rows[i];
-		char_limit = n_board->size.x - 2;
-		text_len = ft_min(ft_nbrlen_base(i, 10) + 1, 3); // digits row '#123'
-		text_len += ft_nbrlen_base(row->cur_amount, 10) + 4; // ' 123: '
-		char_limit -= ft_min(row->cur_amount, char_limit - text_len);
-		mvwprintw(n_board->win,
-		          yoffset,
-		          xoffset,
-		          "#%-2zu %i:",
-		          i + 1,
-		          row->cur_amount);
-		xoffset = n_board->size.x - 2;
-		while (xoffset > char_limit) {
-			mvwprintw(n_board->win, yoffset, xoffset--, "|");
+		// get max character that find in the windows
+		max_char_x = n_board->size.x - 2; // -1 for borders R, -1 for index 0
+		// calculate length of leading text
+		text_len = ft_max(ft_nbrlen_base(i, 10) + 1, 3); // '#123' min:'#1 '
+		text_len += ft_nbrlen_base(row->cur_amount, 10) + 4; // '  123: '
+		// calculate  maximal available space for | characters
+		stop_x = max_char_x - ft_min(row->cur_amount, max_char_x - text_len);
+		// print leading text
+		mvwprintw(n_board->win, yoffset, xoffset,
+			"#%-2zu  %i:", i + 1, row->cur_amount);
+		// adjustment for too many pieces in row
+		if (row->cur_amount > max_char_x - text_len) {
+			stop_x += 9; // offset for number of filler characters	
+			mvwprintw(n_board->win, yoffset, xoffset + text_len, "|||| ... ");
+		}
+		// begin '|' print at max possible X coordinate
+		// xoffset = max_char_x;
+		// while (xoffset > stop_x) {
+		// alternative approach from left to right
+		xoffset += stop_x;
+		while (xoffset < max_char_x) {
+			mvwprintw(n_board->win, yoffset, xoffset++, "|");
 		}
 		yoffset++;
 		i++;
 	}
 	wrefresh(n_board->win);
 }
-
-// void	handle_mouse_event(t_ncurses *env, int key, MEVENT *mouse)
-// {
-// 	// scrolling for each window
-// }
-
-// void	run_visuals(t_board *board)
-// Result	run_visuals(t_board *board)
-// {
-// 	t_ncurses	env;
-// 	// MEVENT		mouse;
-// 	int key = 0;
-
-// 	setup_ncurses(&env);
-// 	// inital draw windows?
-// 	test_draw(&env);
-// 	while (key != 'q' && key != 27)
-// 	{
-// 		// input window
-// 		// handle_input_window(&env, key);
-// 		// history window
-// 		// handle_history_window(&env, key);
-// 		// board window
-// 		update_board(&env.board, board);
-// 		// if (key == KEY_MOUSE) {
-// 		// 	getmouse(&mouse);
-// 		// 	handle_mouse_event(&env, key, &mouse);
-// 		// }
-// 		key = wgetch(env.input.win);
-// 	}
-// 	cleanup_ncruses(&env);
-// 	return (RESULT_OK);
-// }
-
-// int main(void)
-// {
-// 	run_visuals();
-// 	return (0);
-// }
