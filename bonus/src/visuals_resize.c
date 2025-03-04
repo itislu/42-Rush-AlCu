59c53958bd4ad95c3fd591f8c8eb473ef40778e7
dltslt <87982398+dltslt@users.noreply.github.com>
2025-03-04 11:25:36 +0100

    fix: clear leftover characters on resize

--- bonus/src/visuals_resize.c
+++ bonus/src/visuals_resize.c
@@ -7,16 +7,18 @@ Result	resize_window(int ch, t_ncurses *env, t_board *board)
 	if (ch != KEY_RESIZE)
 		return (RESULT_OK);
 	// destroy old environment
+	erase(); // clear the standard screen => removes borders lefthand side
 	cleanup_ncruses(env);
-	// creaaate new environment
+	// create new environment
 	res = setup_ncurses(env);
 	if (res != RESULT_OK)
 		return (res);
 	// fill with content
 	(void) board;
 	// redraw new windows
+	refresh(); // refreshes standard screen => removes borders righthand side
 	wrefresh(env->board.win);
 	wrefresh(env->history.win);
-	wrefresh(env->input.win);
+	// wrefresh(env->input.win); // this is drawn somewhere else too
 	return (RESULT_OK);
 }
