17a7a4a894a702cf5514f9705218b03a3966d822
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-03-07 19:33:27 +0100

    fix: Stop program when CTRL+D also in board building

--- bonus/inc/prompt.h
+++ bonus/inc/prompt.h
@@ -8,5 +8,5 @@ Result prompt_picks(t_board *board, unsigned int *picks);
 
 /* prompt_utils */
 Result get_input(char **line, int fd);
-Result read_file(char **line, int fd);
+Result read_line(char **line, int fd);
 bool is_valid_number(const char *num, unsigned int lower, unsigned int upper);
