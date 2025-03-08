ff30a873aa0a640028e11b62d34b41f64602db3a
dltslt <87982398+dltslt@users.noreply.github.com>
2025-03-08 19:13:01 +0100

    fix: content update in scrolled window on terminal height increase

--- bonus/src/visuals_resize.c
+++ bonus/src/visuals_resize.c
@@ -13,8 +13,17 @@ Result	resize_window(int ch, t_ncurses *env, t_board *board)
 	res = setup_ncurses(env);
 	if (res != RESULT_OK)
 		return (res);
-	// redraw new windows
 	refresh(); // refresh standard screen => remove content right&down (shrink)
+	// adjust scroll offset to show more lines when increasing window size
+	if (env->board.scroll_offset
+		&& env->board.size.y - 3 > board->cur_row - env->board.scroll_offset) {
+			env->board.scroll_offset--;
+		}
+	if (env->history.scroll_offset
+		&& env->history.size.y - 3 > board->cur_turn - env->history.scroll_offset) {
+			env->history.scroll_offset--;
+		}
+	// redraw new windows
 	update_board(&env->board, board);
 	update_history(&env->history, board);
 	update_input(&env->input, board);
