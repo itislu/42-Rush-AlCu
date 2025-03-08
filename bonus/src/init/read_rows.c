itislu/42-Rush-AlCu

commit 54a236356f05b90ead6d0a7d1e55f3a53d80c1d4

fix: Accept EOF in non-interactive mode anywhere to end board creation
The issue was when reading from stdin, we didn't know if it's interactive and want double newline to finish the board creation, or if it's coming from a file redirect and we accept EOF anywhere.

I added a function which should reliably detect if `STDIN_FILENO` is interactive or not, with the limited functions we are allowed to use.

With this I was also able to get rid of the global `g_stdin` variable.

bonus/src/init/read_rows.c
@@ -10,40 +10,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static Result read_file(t_list **rows, int fd);
+static Result
+read_file(t_list **rows, int fd, Result (*read_line)(char **, int));
 static bool is_input_end(const char *line);
 static t_row *new_row(const char *line);
+static bool is_interactive(void);
+static Result make_interactive(void);
 
 Result read_rows(t_list **rows, const char *filename)
 {
-	int fd = g_stdin;
+	Result res = RESULT_OK;
+	const bool interactive = is_interactive();
 
 	if (filename != NULL) {
-		fd = open(filename, O_RDONLY);
+		int fd = open(filename, O_RDONLY);
 		if (fd == -1) {
 			return INTERNAL_ERROR;
 		}
+		res = read_file(rows, fd, read_line_file);
+		close(fd);
 	}
-	else {
+	else if (!interactive) {
+		res = read_file(rows, STDIN_FILENO, read_line_file);
+	}
+	else { // interactive
 		const char title[] = "🔨 BOARD CREATION 🔨";
 		print_boxed_specialstr(title, sizeof(title) - 1 - 4);
 		ft_printf(
 		    "Enter board (one number 1-10000 per line, empty line to end):\n");
+		res = read_file(rows, STDIN_FILENO, read_line_user);
+		if (res == RESULT_OK) {
+			clear_rows(1);
+			move_down_a_line();
+		}
 	}
-	Result res = read_file(rows, fd);
 
-	close(fd);
-	if (fd != g_stdin) {
-		close(g_stdin);
-	}
-	g_stdin = open("/dev/tty", O_RDONLY);
-	if (g_stdin == -1) {
-		res = INTERNAL_ERROR;
+	if (!interactive) {
+		res = make_interactive();
 	}
 	return res;
 }
 
-static Result read_file(t_list **rows, int fd)
+static Result
+read_file(t_list **rows, int fd, Result (*read_line)(char **, int))
 {
 	Result res = RESULT_OK;
 	char *line = NULL;
@@ -51,10 +60,6 @@ static Result read_file(t_list **rows, int fd)
 
 	while ((res = read_line(&line, fd)) == RESULT_OK) {
 		if (is_input_end(line)) {
-			if (fd == g_stdin) {
-				clear_rows(1);
-				move_down_a_line();
-			}
 			break;
 		}
 		t_row *row = new_row(line);
@@ -75,7 +80,7 @@ static Result read_file(t_list **rows, int fd)
 
 static bool is_input_end(const char *line)
 {
-	return (ft_strcmp(line, "\n") == 0);
+	return (line == NULL || ft_strcmp(line, "\n") == 0);
 }
 
 static t_row *new_row(const char *line)
@@ -91,4 +96,53 @@ static t_row *new_row(const char *line)
 	row->start_amount = ft_atoi(line);
 	row->cur_amount = row->start_amount;
 	return row;
+}
+
+/**
+ * Detects if STDIN_FILENO is interactive by checking if its 'open-flags' are
+ * 'O_RDWR | O_APPEND'.
+ *
+ * The fdinfo of a normal stdin looks like this:
+ *     pos:    0
+ *     flags:  02002
+ *     mnt_id: 27
+ *     ino:    17
+ * A redirected stdin looks like this:
+ *     pos:    0
+ *     flags:  0100000
+ *     mnt_id: 25
+ *     ino:    23350703
+ */
+static bool is_interactive(void)
+{
+	const int info_fd = open("/proc/self/fdinfo/0", O_RDONLY);
+	if (info_fd == -1) {
+		return false;
+	}
+
+	bool res = false;
+	char *line = NULL;
+	while (read_line_file(&line, info_fd) == RESULT_OK) {
+		if (ft_strnstr(line, "flags:", ft_strlen(line)) != NULL) {
+			const char *end = ft_strrchr(line, '2');
+			if (end != NULL && !ft_isdigit(end[1]) && end[-3] == '2') {
+				res = true;
+			}
+			break;
+		}
+		free(line);
+	}
+	free(line);
+	free_get_next_line();
+	close(info_fd);
+	return res;
+}
+
+static Result make_interactive(void)
+{
+	close(STDIN_FILENO);
+	if (open("/dev/tty", O_RDONLY) == -1) { // STDIN_FILENO gets closed at exit
+		return INTERNAL_ERROR;
+	}
+	return RESULT_OK;
 }

src/init/read_rows.c
@@ -10,40 +10,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static Result read_file(t_list **rows, int fd);
+static Result
+read_file(t_list **rows, int fd, Result (*read_line)(char **, int));
 static bool is_input_end(const char *line);
 static t_row *new_row(const char *line);
+static bool is_interactive(void);
+static Result make_interactive(void);
 
 Result read_rows(t_list **rows, const char *filename)
 {
-	int fd = g_stdin;
+	Result res = OK;
+	const bool interactive = is_interactive();
 
 	if (filename != NULL) {
-		fd = open(filename, O_RDONLY);
+		int fd = open(filename, O_RDONLY);
 		if (fd == -1) {
 			return INTERNAL_ERROR;
 		}
+		res = read_file(rows, fd, read_line_file);
+		close(fd);
 	}
-	else {
+	else if (!interactive) {
+		res = read_file(rows, STDIN_FILENO, read_line_file);
+	}
+	else { // interactive
 		const char title[] = "🔨 BOARD CREATION 🔨";
 		print_boxed_specialstr(title, sizeof(title) - 1 - 4);
 		ft_printf(
 		    "Enter board (one number 1-10000 per line, empty line to end):\n");
+		res = read_file(rows, STDIN_FILENO, read_line_user);
+		if (res == OK) {
+			clear_rows(1);
+			move_down_a_line();
+		}
 	}
-	Result res = read_file(rows, fd);
 
-	close(fd);
-	if (fd != g_stdin) {
-		close(g_stdin);
-	}
-	g_stdin = open("/dev/tty", O_RDONLY);
-	if (g_stdin == -1) {
-		res = INTERNAL_ERROR;
+	if (!interactive) {
+		res = make_interactive();
 	}
 	return res;
 }
 
-static Result read_file(t_list **rows, int fd)
+static Result
+read_file(t_list **rows, int fd, Result (*read_line)(char **, int))
 {
 	Result res = OK;
 	char *line = NULL;
@@ -51,10 +60,6 @@ static Result read_file(t_list **rows, int fd)
 
 	while ((res = read_line(&line, fd)) == OK) {
 		if (is_input_end(line)) {
-			if (fd == g_stdin) {
-				clear_rows(1);
-				move_down_a_line();
-			}
 			break;
 		}
 		t_row *row = new_row(line);
@@ -75,7 +80,7 @@ static Result read_file(t_list **rows, int fd)
 
 static bool is_input_end(const char *line)
 {
-	return (ft_strcmp(line, "\n") == 0);
+	return (line == NULL || ft_strcmp(line, "\n") == 0);
 }
 
 static t_row *new_row(const char *line)
@@ -91,4 +96,53 @@ static t_row *new_row(const char *line)
 	row->start_amount = ft_atoi(line);
 	row->cur_amount = row->start_amount;
 	return row;
+}
+
+/**
+ * Detects if STDIN_FILENO is interactive by checking if its 'open-flags' are
+ * 'O_RDWR | O_APPEND'.
+ *
+ * The fdinfo of a normal stdin looks like this:
+ *     pos:    0
+ *     flags:  02002
+ *     mnt_id: 27
+ *     ino:    17
+ * A redirected stdin looks like this:
+ *     pos:    0
+ *     flags:  0100000
+ *     mnt_id: 25
+ *     ino:    23350703
+ */
+static bool is_interactive(void)
+{
+	const int info_fd = open("/proc/self/fdinfo/0", O_RDONLY);
+	if (info_fd == -1) {
+		return false;
+	}
+
+	bool res = false;
+	char *line = NULL;
+	while (read_line_file(&line, info_fd) == OK) {
+		if (ft_strnstr(line, "flags:", ft_strlen(line)) != NULL) {
+			const char *end = ft_strrchr(line, '2');
+			if (end != NULL && !ft_isdigit(end[1]) && end[-3] == '2') {
+				res = true;
+			}
+			break;
+		}
+		free(line);
+	}
+	free(line);
+	free_get_next_line();
+	close(info_fd);
+	return res;
+}
+
+static Result make_interactive(void)
+{
+	close(STDIN_FILENO);
+	if (open("/dev/tty", O_RDONLY) == -1) { // STDIN_FILENO gets closed at exit
+		return INTERNAL_ERROR;
+	}
+	return OK;
 }

src/main.c
@@ -10,8 +10,6 @@
 static Result game_loop(t_board *board);
 static bool is_game_end(t_board *board);
 
-int g_stdin = STDIN_FILENO;
-
 int main(int argc, char *argv[])
 {
 	Result res = OK;
@@ -56,6 +54,7 @@ int main(int argc, char *argv[])
 	}
 
 	free_board(&board);
+	close(STDIN_FILENO);
 	return res;
 }
 

bonus/src/main.c
@@ -9,8 +9,6 @@
 #include <sys/param.h>
 #include <unistd.h>
 
-int g_stdin = STDIN_FILENO;
-
 int main(int argc, char *argv[])
 {
 	Result res = RESULT_OK;
@@ -58,5 +56,6 @@ int main(int argc, char *argv[])
 	}
 
 	free_board(&board);
+	close(STDIN_FILENO);
 	return res;
 }

bonus/inc/alcu.h
@@ -21,8 +21,6 @@
               ╰───────────────────────╯\n\
 "
 
-extern int g_stdin;
-
 typedef enum Result {
 	RESULT_OK,
 	USAGE_ERROR,

bonus/inc/prompt.h
@@ -7,6 +7,6 @@ Result prompt_game_mode(Mode *mode);
 Result prompt_picks(t_board *board, unsigned int *picks);
 
 /* prompt_utils */
-Result get_input(char **line, int fd);
-Result read_line(char **line, int fd);
+Result read_line_user(char **line, int fd);
+Result read_line_file(char **line, int fd);
 bool is_valid_number(const char *num, unsigned int lower, unsigned int upper);

bonus/src/init/init.c
@@ -36,7 +36,6 @@ void free_board(t_board *board)
 	}
 	free(board->picks);
 	free_get_next_line();
-	close(g_stdin);
 }
 
 static Result read_board(t_board *board, const char *filename)

bonus/src/prompt/prompt.c
@@ -20,7 +20,7 @@ Result prompt_game_mode(Mode *mode)
 		    "Select a game mode:\n%i - Last to pick loses\n%i - Last to pick wins\n",
 		    LAST_LOSES,
 		    LAST_WINS);
-		res = get_input(&line, g_stdin);
+		res = read_line_user(&line, STDIN_FILENO);
 		if (res != RESULT_OK) {
 			break;
 		}
@@ -56,7 +56,7 @@ Result prompt_picks(t_board *board, unsigned int *picks)
 
 	while (true) {
 		ft_printf("Pick up to %u%s:\n", max_picks, PRT_PIECE);
-		res = get_input(&line, g_stdin);
+		res = read_line_user(&line, STDIN_FILENO);
 		if (res != RESULT_OK) {
 			break;
 		}

commit 71b21e93a5c9a366f3ac0a4d966077919a160d7d

fix: Fix missing empty line before board printout when read from file
Bug was introduced in commit 54a236356f05b90ead6d0a7d1e55f3a53d80c1d4.

bonus/src/init/read_rows.c
@@ -41,7 +41,6 @@ Result read_rows(t_list **rows, const char *filename)
 		res = read_file(rows, STDIN_FILENO, read_line_user);
 		if (res == RESULT_OK) {
 			clear_rows(1);
-			move_down_a_line();
 		}
 	}
 

src/init/read_rows.c
@@ -41,7 +41,6 @@ Result read_rows(t_list **rows, const char *filename)
 		res = read_file(rows, STDIN_FILENO, read_line_user);
 		if (res == OK) {
 			clear_rows(1);
-			move_down_a_line();
 		}
 	}
 

src/main.c
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
 		res = init_board(&board, argv[1]);
 	}
 	if (res == OK) {
+		ft_printf("\n");
 		print_board_complete(&board);
 		ft_printf("\n");
 		res = prompt_game_mode(&board.game_mode);